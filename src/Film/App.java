2b15742316194a4fdfdb33c708059c77d12cc729
Amy_oyzl <32362887+Amyoyzl@users.noreply.github.com>
2017-11-27 20:37:28 +0800

    详细内容

--- src/Film/App.java
+++ src/Film/App.java
@@ -1,7 +1,10 @@
 package Film;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+
 /**
  * 
  * @author Amy_oyzl
@@ -10,59 +13,88 @@ import java.util.Scanner;
 public class App {
 
 	Scanner in = new Scanner(System.in);
-	
+
 	ArrayList<Film> films = new ArrayList<>();
 
 	public static void main(String[] args) {
 
 		App app = new App();
 		app.work();
+		System.out.println("END!");
 	}
 
 	public void work() {
 		int choice;
 		outer: while (true) {
+			System.out.println();
 			printMenu();
 			choice = in.nextInt();
 			switch (choice) {
 			case 1:
+				System.out.println();
 				loadIn();
 				break;
 			case 2:
+				System.out.println();
 				printScore();
 				break;
 			case 3:
+				System.out.println();
 				printTime();
 				break;
 			case 4:
 				break outer;
-			default: break outer;
+			default:
+				break outer;
 			}
 		}
 	}
 
 	public void loadIn() {
-
+		Film f = new Film();
+		System.out.println("录入电影");
+		System.out.print("请输入片名：");
+		f.setTitle(in.next());
+		System.out.print("请输入上映时间：");
+		f.setYear(in.nextInt());
+		System.out.print("请输入评分：");
+		f.setScore(in.nextDouble());
+		films.add(f);
+		System.out.print("是否继续录入（yes/no）：");
+		if (in.next().equalsIgnoreCase("yes"))
+			loadIn();
 	}
 
 	private void printScore() {
-		
-
+		if(films.size()==0) {
+			System.out.println("暂无内容！");
+			return;
+		}
+		System.out.println("豆瓣评分");
+		for (Film e : films) {
+			System.out.println(e.getTitle() + ":" + e.getScore());
+		}
 	}
 
 	private void printTime() {
-		
-
+		if(films.size()==0) {
+			System.out.println("暂无内容！");
+			return;
+		}
+		System.out.println("上映时间");
+		for (Film e : films) {
+			System.out.println(e.getTitle() + ":" + e.getYear());
+		}
 	}
 
 	public void printMenu() {
-		System.out.println("----------------------");
-		System.out.println("      1               录入");
+		System.out.println("----------------------------");
+		System.out.println("      1     录入");
 		System.out.println("      2     豆瓣评分浏览");
-		System.out.println("      3    上映时间浏览");
-		System.out.println("      4    退出");
-		System.out.println("----------------------");
-		System.out.println("请输入功能（1-4）：");
+		System.out.println("      3     上映时间浏览");
+		System.out.println("      4     退出");
+		System.out.println("----------------------------");
+		System.out.print("请输入功能（1-4）：");
 	}
 
 }
--- src/Film/Film.java
+++ src/Film/Film.java
@@ -4,5 +4,33 @@ public class Film {
 	private String title;
 	private int year;
 	private double score;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public double getScore() {
+		return score;
+	}
+
+	public void setScore(double score) {
+		this.score = score;
+	}
+
+	public void print() {
+		System.out.println(title + "   " + year + "   " + score);
+	}
 	
 }
